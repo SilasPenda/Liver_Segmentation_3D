eec1d3741350ea086526ac879236ca0973f0320d
Silas-Terkuma Penda <sp165339@krenek03.u-bourgogne.fr>
2024-11-28 22:28:00 +0100

    Jobscript changes

--- jobscript.sh
+++ jobscript.sh
@@ -2,20 +2,24 @@
 #$ -q gpu
 #$ -m abe
 #$ -M sp165339@etu.u-bourgogne.fr
-#$ -N kewl_script_executor
+#$ -N segm_script_executor
 #$ -o worklog.log
+
+export PYTHONUSERBASE=/work/c-2iia/sp165339/venv
+export PATH=/work/c-2iia/sp165339/venv/bin
+export PYTHONIOENCODING=utf8
  
 ## job start time
-printf "\n Job started at : $(date)\n-----\n\n"
+## printf "\n Job started at : $(date)\n-----\n\n"
  
 ## what do we need? modules, envs etc.
 module purge ## clear out the current module list
-# module load python/3.10
-export PYTHONUSERBASE=/work/c-2iia/sp165339/LS3D_venv
+module load python/3.9.10
+## source /work/c-2iia/sp165339/venv/bin/activate
 module load pytorch/2.0.0/gpu
 module list
 
-printf "\n --- begin of python execution ---\n"
+## printf "\n --- begin of python execution ---\n"
 
 ## set a work dir OR variables with path to feed to python (ARGPARSER, click, etc)
 cd /work/c-2iia/sp165339/Liver_Segmentation_3D
@@ -24,7 +28,7 @@ cd /work/c-2iia/sp165339/Liver_Segmentation_3D
 python train.py
 
 
-printf "\n --- end of python execution ---\n\n"
+## printf "\n --- end of python execution ---\n\n"
 
 ## job end time
-printf "----\njob ended at $(date)\n\n"
+## printf "----\njob ended at $(date)\n\n"
